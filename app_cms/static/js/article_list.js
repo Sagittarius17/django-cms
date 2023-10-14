5980a010c60e6f7bbc6f018d4966979bebd7dd80
shuvendu <108450702+Sagittarius17@users.noreply.github.com>
2023-10-14 14:59:46 +0530

    layout

---
+++ app_cms/static/js/article_list.js
@@ -0,0 +1,10 @@
+let isAuthenticated = {{ is_authenticated|lower }};
+const newArticle = document.getElementById('new_article');
+const articleCrud = document.getElementById('article_crud');
+
+if (isAuthenticated) {
+newArticle.style.display = "flex";
+} else {
+newArticle.style.display = "none";
+articleCrud.style.display = "none";
+}
